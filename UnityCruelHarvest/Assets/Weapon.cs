019c16539083a510fae22e27ab8525c2fb4b4afd
yayabingbong <97674510+sauftot@users.noreply.github.com>
2023-07-23 20:00:07 +0200

    Started basic weapon functionality

--- UnityCruelHarvest/Assets/Weapon.cs
+++ UnityCruelHarvest/Assets/Weapon.cs
@@ -9,9 +9,21 @@ public class Weapon : Item
     public int magazineSize;
     public int magazineCount;
     public int currentAmmo;
+    bool readyToShoot;
+    public int shootCooldown;
+    public int reloadCooldown;
+    public int weaponDamage;
+    bool isScoped = false;
+
+
+    [Header("Keybinds")]
+    public KeyCode shootKey = KeyCode.Mouse0;
+    public KeyCode reloadKey = KeyCode.R;
+    public KeyCode scopeKey = KeyCode.Mouse1;
+
 
     public float range;
-    public WeaponType weaptonType;
+    public WeaponType weaponType;
 
     public enum WeaponType{
         Pistol,
@@ -19,6 +31,65 @@ public class Weapon : Item
         Shotgun,
         Sniper,
         Melee
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void MyInput()
+    {
+        if (Input.GetKey(shootKey) && readyToShoot && currentAmmo > 0)
+        {
+            if (currentAmmo > 0)
+            {
+                readyToShoot = false;
+                Attack();
+                //Invoke(nameof(ResetShot), shootCooldown);
+            }
+
+
+        }
+        if (Input.GetKey(reloadKey) && readyToShoot)
+        {
+            if (magazineCount > 0)
+            {
+                readyToShoot = false;
+                Reload();
+                //Invoke(nameof(ResetShot), shootCooldown);
+            }
+        }
+        if (Input.GetKeyDown(scopeKey) && !isScoped && readyToShoot)
+        {
+            ToggleScope();
+        }
+        if (Input.GetKeyUp(scopeKey) && isScoped)
+        {
+            ToggleScope();
+        }
+    }
+
+    private void Attack()
+    {
+        currentAmmo--;
+    }
+
+    private void Reload()
+    {
+        currentAmmo = magazineSize;
+        magazineCount--;
+           
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+
+    private void ToggleScope()
+    {
+        isScoped = !isScoped;
     }
 
 
