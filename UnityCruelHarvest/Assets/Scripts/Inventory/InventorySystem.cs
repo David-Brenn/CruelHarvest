fb29c6424ae848b2906a2a22e6357811c303f31b
David Brenn <davidbrenn@t-online.de>
2023-07-23 21:36:57 +0200

    Add InventorySystem.cs
    Signed-off-by: David Brenn <davidbrenn@t-online.de>

---
+++ UnityCruelHarvest/Assets/Scripts/Inventory/InventorySystem.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySystem : MonoBehaviour
+{
+    public static InventorySystem inventorySystem;
+    public List<Item> inventory;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(inventorySystem != null){
+            inventorySystem = this;
+        } else {
+            Destroy(this);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addItem(Item item){
+        inventory.Add(item);
+    }
+
+    public void removeItem(Item item){
+        inventory.Remove(item);
+    }
+
+    public void useItem(Item item){
+        if(inventory.Contains(item))  {
+            item.Use();
+        }
+    }
+    /// <summary>
+    /// Spawns the prefab of a item if the player want to drop a item
+    /// </summary>
+    /// <param name="item"></param>
+    public void dropItem(Item item){
+
+        //Check if item is droppable (is a weapon)
+        //If so spawn the prefab a the position of the player
+        
+    }
+}
